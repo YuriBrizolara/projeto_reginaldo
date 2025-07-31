d5f2e71de7482cb0308d58852268aab629b58f6f
YuriBrizolara <yuribrizolara22@gmail.com>
2025-07-31 14:34:05 -0300

    criando requisitos para o cadastro de usuario

---
+++ src/validacoes/schemaCadastroUsuario.js
@@ -0,0 +1,25 @@
+const joi = require('joi');
+
+const schemaCadastroUsuario = joi.object({
+    nome: joi.string().required().messages({
+        'any.required': 'O campo nome é obrigatório',
+        'string.base': 'O campo nome é obrigatório',
+        'string.empty': 'O campo nome é obrigatório',
+    }),
+
+    email: joi.string().email().required().messages({
+        'string.email': 'O campo email precisa ter um formato válido',
+        'string.base': 'O campo email é obrigatório',
+        'any.required': 'O campo email é obrigatório',
+        'string.empty': 'O campo email é obrigatório',
+    }),
+
+    senha: joi.string().min(6).required().messages({
+        'any.required': 'O campo senha é obrigatório',
+        'string.base': 'O campo senha é obrigatório',
+        'string.empty': 'O campo senha é obrigatório',
+        'string.min': 'A senha precisa conter, no mínimo, 6 caracteres',
+    }),
+});
+
+module.exports = schemaCadastroUsuario;
