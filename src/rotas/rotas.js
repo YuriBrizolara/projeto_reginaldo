f4dac880185215a7e67485be813e76401d30b892
YuriBrizolara <yuribrizolara22@gmail.com>
2025-08-04 11:30:56 -0300

    primeiras configurações do arquivo rotas

--- src/rotas/rotas.js
+++ src/rotas/rotas.js
@@ -1,1 +1,29 @@
 const express = require('express');
+const {
+    cadastrarUsuario,
+    editarUsuario,
+    detalharUsuario,
+} = require('../controladores/usuarios');
+
+const efetuarLogin = require('../controladores/login');
+
+const schemaCadastroUsuario = require('../validacoes/schemaCadastroUsuario');
+const {
+    validarCorpo,
+    verificarToken,
+} = require('../intermediarios/validarCorpo');
+const validarParametroDeRota = require('../intermediarios/validarParametrosRota');
+const validarCpf_Email = require('../intermediarios/validarCpf');
+const rotas = express.Router();
+rotas.post('/usuario', validarCorpo(schemaCadastroUsuario), cadastrarUsuario);
+rotas.post('/login', validarCorpo(schemaLogin), efetuarLogin);
+rotas.use(verificarToken);
+
+rotas.put(
+    '/usuario',
+    validarCorpo(schemaCadastroUsuario),
+    validarCpf_Email,
+    editarUsuario
+);
+rotas.get('/usuario', detalharUsuario);
+module.exports = rotas;
