92131926ba86275f0a4a1d3b166252f58504b1a0
YuriBrizolara <yuribrizolara22@gmail.com>
2025-08-04 15:01:33 -0300

    criando a arquivo para conexão das rotas. Criando o primeiro sistema de login e cadastro de usuarios

---
+++ src/controladores/usuarios.js
@@ -0,0 +1,80 @@
+const knex = require('../conexão');
+const bcrypt = require('bcrypt');
+
+const cadastrarUsuario = async (req, res) => {
+    const { nome, email, senha } = req.body;
+
+    try {
+        const emailExiste = await knex('usuarios').where({ email }).first();
+
+        if (emailExiste) {
+            return res.status(400).json({ mensagem: 'Esse email já existe' });
+        }
+
+        const senhaCriptografada = await bcrypt.hash(senha, 10);
+
+        const novoUsuario = await knex('usuarios')
+            .insert({
+                nome,
+                email,
+                senha: senhaCriptografada,
+            })
+            .returning(['id', 'nome', 'email']);
+
+        return res.status(201).json(novoUsuario);
+    } catch (error) {
+        return res.status(500).json({ mensagem: 'Erro interno do Servidor!' });
+    }
+};
+const detalharUsuario = async (req, res) => {
+    try {
+        return res.status(200).json(req.usuario);
+    } catch (error) {
+        return res.status(500).json({ mensagem: 'Erro interno do Servidor!' });
+    }
+};
+const editarUsuario = async (req, res) => {
+    const { nome, email, senha } = req.body;
+    const { id } = req.usuario;
+
+    try {
+        const usuarioExiste = await knex('usuarios').where({ id }).first();
+
+        if (!usuarioExiste) {
+            return res.status(404).json({ mensagem: 'Usuario não encontrado' });
+        }
+
+        const senhaCriptografada = await bcrypt.hash(senha, 10);
+
+        if (email !== req.usuario.email) {
+            const emailUsuarioExiste = await knex('usuarios')
+                .where({ email })
+                .first();
+
+            if (emailUsuarioExiste) {
+                return res
+                    .status(400)
+                    .json({ mensagem: 'O e-mail já existe.' });
+            }
+        }
+
+        const atualizarUsuario = await knex('usuarios')
+            .where({ id })
+            .update({
+                nome,
+                email,
+                senha: senhaCriptografada,
+            })
+            .returning(['id', 'nome', 'email']);
+
+        return res.status(200).send(atualizarUsuario);
+    } catch (error) {
+        return res.status(500).json({ mensagem: 'Erro interno do Servidor!' });
+    }
+};
+
+module.exports = {
+    cadastrarUsuario,
+    detalharUsuario,
+    editarUsuario,
+};
